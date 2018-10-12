0240ba1017b5c2edbe9e18731d8566b6a0e1505a
shaoding <shaodingzhang@gmail.com>
2018-10-12 16:30:48 +0800

    add test data

---
+++ test/path-const.ts
@@ -0,0 +1,10 @@
+const USER = {
+    LIST: '/api/client/user/list',
+    ROLES: '/api/client/user/roles',
+    ADD: '/api/client/user/add',
+    DELETE: '/api/client/user/delete',
+}
+
+export const API_URL = {
+    USER,
+}
